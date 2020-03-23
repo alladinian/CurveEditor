146fd623e05a7eed8aadfa143063da008764801b
Vasilis Akoinoglou <alladinian@gmail.com>
2020-03-23 23:10:07 +0200

    Initial Commit

---
+++ CurveEditor/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CurveEditor
+//
+//  Created by Vasilis Akoinoglou on 23/3/20.
+//  Copyright © 2020 Vasilis Akoinoglou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
